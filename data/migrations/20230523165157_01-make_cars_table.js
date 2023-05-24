def4ee2ed12f653cd700be4c855258cf7e59037d
cosmopoliitans <elfkyn52@gmail.com>
2023-05-24 23:21:41 +0300

    görevler tamamlandı

---
+++ data/migrations/20230523165157_01-make_cars_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (table) => {
+    table.increments();
+    table.string("vin").notNullable().unique();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.string("title");
+    table.string("transmission");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
